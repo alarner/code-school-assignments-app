554f02c85bfd4deefd3debf1d2bb37c60b25b81a
Aaron Larner <anlarner@gmail.com>
2015-03-14 20:15:10 -0500

    Update connections.js

--- config/connections.js
+++ config/connections.js
@@ -34,7 +34,7 @@ module.exports.connections = {
     adapter: 'sails-postgresql',
     host: 'localhost',
     user: 'postgres',
-    password: 'postgres',
+    password: '',
     database: 'appdb'
   }
 
