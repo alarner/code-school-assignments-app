5eb266213b45e601a262f3ee10e64982f85d2464
Aaron Larner <anlarner@gmail.com>
2015-05-23 08:27:21 -0500

    Add gulp serve

---
+++ gulp/watch.js
@@ -0,0 +1,5 @@
+module.exports = [
+	'api/**/*.js',
+	'config/**/*.js',
+	'package.json'
+];
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,23 @@
 var gulp = require('gulp');
 var scripts = require('./gulp/scripts');
 var styles = require('./gulp/styles');
+var watch = require('./gulp/watch');
 var _ = require('lodash');
+var spawn = require('child_process').spawn;
 
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
+var server = null;
+var serverRunning = false;
 
-gulp.task('default', ['js', 'concat-css', 'sass', 'css-sass', 'templates']);
+/*
+ * BUILD TASKS
+ */
+
+gulp.task('build', ['js', 'concat-css', 'sass', 'css-sass', 'templates']);
 
 gulp.task('js', function() {
 	var scriptPaths = _.map(scripts, function(script) {
@@ -56,4 +64,52 @@ gulp.task('images', function () {
 gulp.task('bower_components', function () {
 	return gulp.src('assets/bower_components/**/*')
 	.pipe(gulp.dest('www/bower_components'));
+});
+
+/*
+ * SERVE TASKS
+ */
+
+gulp.task('serve', ['sass-watch', 'watch', 'server']);
+
+gulp.task('sass-watch', function() {
+	gulp.watch('assets/styles/**/*.scss', ['sass-dev']);
+});
+
+gulp.task('sass-dev', function() {
+	return gulp.src('assets/styles/importer.scss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('watch', function() {
+	console.log('watching...', watch);
+	gulp.watch(watch, ['server']);
+});
+
+gulp.task('server', function() {
+	function startServer() {
+		if(server && serverRunning) {
+			console.log('Stopping server...');
+			server.kill();
+		}
+		else {
+			console.log('Starting server...');
+			server = spawn('sails', ['lift', '--prod']);
+			serverRunning = true;
+			server.on('close', function(code) {
+				console.log('Server closed with code ['+code+']');
+				serverRunning = false;
+				startServer();
+			});
+			server.stdout.on('data', function(data) {
+				console.log(data.toString());
+			});
+			server.stderr.on('data', function(data) {
+				console.log(data.toString());
+			});
+		}
+	}
+	
+	startServer();
 });
